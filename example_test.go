b3061243f6f48179bd0bf2b0670addf79eb91a13
Kevin Gillette <extemporalgenome@gmail.com>
2015-12-04 01:19:36 -0700

    package-wide example test

--- example_test.go
+++ example_test.go
@@ -11,6 +11,26 @@ import (
 	"github.com/xtgo/set"
 )
 
+func Example() {
+	s := set.Strings([]string{"alpha", "gamma", "alpha"})
+	fmt.Println("set:", s)
+
+	s = set.StringsDo(set.Union, s, "beta")
+	fmt.Println("set + beta:", s)
+
+	fmt.Println(s, "contains any [alpha delta]:",
+		set.StringsChk(set.IsInter, s, "alpha", "delta"))
+
+	fmt.Println(s, "contains all [alpha delta]:",
+		set.StringsChk(set.IsSuper, s, "alpha", "delta"))
+
+	// Output:
+	// set: [alpha gamma]
+	// set + beta: [alpha beta gamma]
+	// [alpha beta gamma] contains any [alpha delta]: true
+	// [alpha beta gamma] contains all [alpha delta]: false
+}
+
 func ExampleUniq() {
 	data := sort.IntSlice{5, 7, 3, 3, 5}
 
